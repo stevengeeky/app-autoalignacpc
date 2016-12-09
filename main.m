bd4762dece76032591ecc85054797d7e1d40146c
Soichi Hayashi <hayashis@iu.edu>
2016-12-09 14:40:18 -0500

    made it to run a test config.json

--- main.m
+++ main.m
@@ -1,15 +1,22 @@
-function t1w_acpcfile = main( config )
+function main( config )
 % Automatically align an T1-weighted image to AC-PC plane
 % 
 % INPUTS:
-%  config.rawT1File   - full path to nifti file that needs to be aligned
-%  config.t1w_acpc    - string used as name for the AC-PC aligned file 
-%  config.acpc_coords - [0,0,0; 0,-16,0; 0,-8,40]
+%  config.t1 - full path to nifti file that needs to be aligned
+%  config.t1_out - string used as name for the AC-PC aligned file 
+%  config.coords - [0,0,0; 0,-16,0; 0,-8,40]
 %
 % Copyright 2017 Franco Pestilli, Indiana University, frakkopesto@gmail.com
 
+disp('loading paths')
+addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
+addpath(genpath('/N/u/hayashis/BigRed2/git/jsonlab'))
+
+% load my own config.json
+config = loadjson('config.json');
+
 % Load the file from disk
-ni = readFileNifti(config.rawT1file);
+ni = niftiRead(config.t1);
 
 % Make sure the file is aligned properly
 ni = niftiApplyCannonicalXform(ni);
@@ -23,11 +30,11 @@ SpatialNormalization = mrAnatComputeSpmSpatialNorm(ni.data, ni.qto_xyz, MNI_temp
 % Assume that the AC-PC coordinates int he template are in a specific location:
 % X, Y, Z = [0,0,0; 0,-16,0; 0,-8,40]
 % Use this assumption and the spatial normalization to extract the corresponding AC-PC location on the raw data
-ImageCoords = mrAnatGetImageCoordsFromSn(SpatialNormalization, tal2mni(config.acpc_coords)', true)';
+ImageCoords = mrAnatGetImageCoordsFromSn(SpatialNormalization, tal2mni(config.coords)', true)';
 
 % Now we assume that ImageCoords contains the AC-PC coordinates that we need for the Raw data. 
 % We will use them to compute the AC_PC alignement automatically. The new file will be saved to disk. 
 % Check the alignement.
-mrAnatAverageAcpcNifti(ni, t1w_acpcfile, ImageCoords, [], [], [], false);
+mrAnatAverageAcpcNifti(ni, config.t1_out, ImageCoords, [], [], [], false);
 
 return
