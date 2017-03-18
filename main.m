e1c0841947407490b920da7bcbcb7a28809792ef
Soichi Hayashi <hayashis@iu.edu>
2017-03-18 19:09:50 -0400

    made coordinate configuration optional

--- main.m
+++ main.m
@@ -34,10 +34,14 @@ MNI_template =  fullfile(mrDiffusionDir, 'templates', 'MNI_T1.nii.gz');
 % Compute the spatial normalization to align the current raw data to the template
 SpatialNormalization = mrAnatComputeSpmSpatialNorm(ni.data, ni.qto_xyz, MNI_template);
 
-% Assume that the AC-PC coordinates int he template are in a specific location:
+% Assume that the AC-PC coordinates in the template are in a specific location:
 % X, Y, Z = [0,0,0; 0,-16,0; 0,-8,40]
 % Use this assumption and the spatial normalization to extract the corresponding AC-PC location on the raw data
-ImageCoords = mrAnatGetImageCoordsFromSn(SpatialNormalization, tal2mni(config.coords)', true)';
+coords = [0,0,0; 0,-16,0; 0,-8,40]
+if isprop(config, 'coords')
+    coords = config.coords
+end
+ImageCoords = mrAnatGetImageCoordsFromSn(SpatialNormalization, tal2mni(coords)', true)';
 
 % Now we assume that ImageCoords contains the AC-PC coordinates that we need for the Raw data. 
 % We will use them to compute the AC_PC alignement automatically. The new file will be saved to disk. 
