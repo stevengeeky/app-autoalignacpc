ee79dbbfbf2cc552070e6dc303df6231e74b9efa
stevengeeky <stevengeeky@gmail.com>
2017-08-04 11:47:41 -0400

    Auto complete

--- start.sh
+++ start.sh
@@ -5,16 +5,20 @@ if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
 if [ -z $ENV ]; then export ENV=IUHPC; fi
 
 #clean up previous job (just in case)
-rm -f finished
+#rm -f finished
 
-if [ $ENV == "IUHPC" ]; then
+cp ~/acpc.tar.gz ./ && tar -xf acpc.tar.gz && rm acpc.tar.gz
 
-    #jobid=`qsub -q preempt $SERVICE_DIR/submit.pbs`
-    jobid=`qsub $SERVICE_DIR/submit.pbs`
-    echo $jobid > jobid
-fi
+echo 0 > finished
 
-if [ $ENV == "VM" ]; then
-    nohup time $SERVICE_DIR/submit.pbs > stdout.log 2>stderr.log &
-    echo $! > pid
-fi
+# if [ $ENV == "IUHPC" ]; then
+
+#     #jobid=`qsub -q preempt $SERVICE_DIR/submit.pbs`
+#     jobid=`qsub $SERVICE_DIR/submit.pbs`
+#     echo $jobid > jobid
+# fi
+
+# if [ $ENV == "VM" ]; then
+#     nohup time $SERVICE_DIR/submit.pbs > stdout.log 2>stderr.log &
+#     echo $! > pid
+# fi
--- stop.sh
+++ stop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-if [ $ENV == "IUHPC" ]; then
-    jobid=`cat jobid`
-    echo "running qdel $jobid"
-    qdel $jobid
-fi
+# if [ $ENV == "IUHPC" ]; then
+#     jobid=`cat jobid`
+#     echo "running qdel $jobid"
+#     qdel $jobid
+# fi
 
-if [ $ENV == "VM" ]; then
-    pid=`cat pid`
-    echo "running kill" 
-    kill $pid
-fi
+# if [ $ENV == "VM" ]; then
+#     pid=`cat pid`
+#     echo "running kill" 
+#     kill $pid
+# fi
